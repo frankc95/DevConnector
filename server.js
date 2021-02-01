d8eed9270ebb0f0a56182b72aafa4594a5fd30c7
frankc95 <blazewicz.j@gmail.com>
2021-02-01 22:16:05 +0000

    Section 2 commit

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Databse 
+connectDB();
+
 // get request to index, callback with request and response
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes 
+// first part links with "router.get('/'," in users.js 
+// and the second part defines the path to file. 
+app.use('/api.users', require('./routes/api/users'));
+app.use('/api.auth', require('./routes/api/auth'));
+app.use('/api.posts', require('./routes/api/posts'));
+app.use('/api.profile', require('./routes/api/profile'));
 
 // It will look for enviroment veriable called PORT || locally on port 5000
 const PORT = process.env.PORT || 5000; 
