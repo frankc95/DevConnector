0505837c685a610a22e4d906860e7169395df22e
frankc95 <blazewicz.j@gmail.com>
2021-02-23 17:22:02 +0000

    most done

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,11 +22,11 @@ const User = require('../../models/User');
 router.post(
   '/',
   [
-    check('name', 'Name is require').not().isEmpty(),
+    check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please, include a valid email').isEmail(),
     check(
       'password',
-      'Please enter a password with 6 or more characters'
+      'Please, enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
